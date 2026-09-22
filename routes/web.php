56dcda5a7a50a9d93ef9d784b605ae8b1fde2cba
Amir Mehrabian <aut0run2011@gmail.com>
2026-09-22 18:37:26 +0330

    Working on the UI

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'dashboard', ['mode' => 'customer'])->name('home');
+Route::view('/portal', 'dashboard', ['mode' => 'customer'])->name('portal');
+Route::view('/admin', 'dashboard', ['mode' => 'admin'])->name('admin');
+Route::view('/login', 'auth', ['type' => 'login'])->name('login');
+Route::view('/register', 'auth', ['type' => 'register'])->name('register');
